cba99676628c9cc9ad37730d0dd1a24302523096
kevin578 <kevinbriggs1@gmail.com>
2018-07-30 20:35:28 -0400

    app is styled

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,16 +1,28 @@
-import React, {Component} from 'react';
-import {View, Text} from 'react-native';
-
+import React, { Component } from "react";
+import { ScrollView } from "react-native";
+import axios from "axios";
+import AlbumDetail from "./AlbumDetail";
 
 class AlbumList extends Component {
-    render(){
-        return (
-            <View>
-                <Text>Album List</Text>
-            </View>
-        )
-    }
+  state = {
+    albums: []
+  };
 
+  componentWillMount() {
+    axios
+      .get("https://rallycoding.herokuapp.com/api/music_albums")
+      .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album => (
+      <AlbumDetail key={album.title} album={album} />
+    ));
+  }
+
+  render() {
+    return <ScrollView>{this.renderAlbums()}</ScrollView>;
+  }
 }
 
 export default AlbumList;
